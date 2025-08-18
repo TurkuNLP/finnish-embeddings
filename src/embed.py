90162868231e0b5299a6b18c65b01e005de07264
Siiri K <siakap@utu.fi>
2025-08-18 16:10:51 +0300

    Add logger and configurable verbosity options

--- src/embed.py
+++ src/embed.py
@@ -1,14 +1,27 @@
 import argparse
+import logging
 from utils.get_data import yield_from_jsonl
 from sentence_transformers import SentenceTransformer
 
+
+def set_up_logger(verbosity_level:int):
+    verbosity_levels = {0: logging.CRITICAL, 1: logging.WARNING, 2: logging.INFO, 3: logging.DEBUG}
+    logging.basicConfig(level=verbosity_levels[verbosity_level], force=True)
+    return logging.getLogger(__name__)
+
+def log_args(args):
+    logger.info(args)
+
 def main(args):
+
+    log_args(args)
 
     model_name = args.model
     data_files = args.data_files
     key = args.key
+    k_documents = args.k
 
-    documents = list(yield_from_jsonl(data_files, key))
+    documents = list(yield_from_jsonl(data_files, key, k_documents))
 
     model = SentenceTransformer(model_name)
     embeddings = model.encode(documents)
@@ -24,6 +37,16 @@ if __name__ == "__main__":
     parser.add_argument("--key",
                         default="text_end",
                         help="The key to the field that should be extracted from each row of JSONL file defined with parameter 'data_files'.")
+    parser.add_argument("--k",
+                        help="Pick first k documents from the JSONL file defined with parameter 'data_files'.")
+    parser.add_argument("--verbosity",
+                        default=3,
+                        choices=[0, 1, 2, 3],
+                        type=int,
+                        help="Logging levels given as an int (the higher the number the more detailed the output): 0 (only critical); 1 (warning); 2 (info); 3 (debug)"
+                        )
     args = parser.parse_args()
+
+    logger = set_up_logger(args.verbosity)
 
     main(args)
