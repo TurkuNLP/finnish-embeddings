2411034a982ea6ac91e202f4af31c4a4e36855fa
Siiri K <siakap@utu.fi>
2025-09-01 13:12:48 +0300

    Resolve string identifier conflict in f-strings

--- config/Config.py
+++ config/Config.py
@@ -48,15 +48,15 @@ class Config:
     def __post_init__(self):
 
         if not self.test:
-            self.save_embeddings_to: str = f"{os.getenv("EMBEDDING_DIR")}/{self.replace_slashes_in_model_name()}_embeddings.npy"
-            self.save_index_to: str = f"{os.getenv("INDEX_DIR")}/{self.replace_slashes_in_model_name()}_index.faiss"
-            self.save_results_to: str = f"{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_results.json" # TODO: Modify once the format is decided
+            self.save_embeddings_to: str = f'{os.getenv("EMBEDDING_DIR")}/{self.replace_slashes_in_model_name()}_embeddings.npy'
+            self.save_index_to: str = f'{os.getenv("INDEX_DIR")}/{self.replace_slashes_in_model_name()}_index.faiss'
+            self.save_results_to: str = f'{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_results.json' # TODO: Modify once the format is decided
 
         # Load 
         else:
-            self.save_embeddings_to: str = f"{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_embeddings_test.npy"
-            self.save_index_to: str = f"{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_index_test.faiss"
-            self.save_results_to: str = f"{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_results_test.json" # TODO: Modify once the format is decided
+            self.save_embeddings_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_embeddings_test.npy'
+            self.save_index_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_index_test.faiss'
+            self.save_results_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}_results_test.json' # TODO: Modify once the format is decided
 
     @classmethod
     def parse_config(cls, args):
