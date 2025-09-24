237eb362c320301c3731f8655da773b9a378f22c
Siiri K <siakap@utu.fi>
2025-09-24 13:05:20 +0300

    New dev-test-split: do not exclude column articles from dev

--- src/dev_test_split.py
+++ src/dev_test_split.py
@@ -3,11 +3,19 @@ import os
 import json
 import numpy as np
 from tqdm import tqdm
+from utils.helpers import get_line_count
 
 load_dotenv()
 
 DATA_PATH = os.getenv("NEWS_DATA_SORTED_BY_LEN")
 DEVTEST_PATH = DATA_PATH.replace(".json", "-dev-test.json")
+
+path_start, devtest_filename = DEVTEST_PATH.rsplit("/", 1)
+dev_indices_filename = "5000_dev_indices_mapped_to_"+devtest_filename
+test_indices_filename = "134313_test_indices_mapped_to_"+devtest_filename
+
+DEV_INDICES_PATH = os.path.join(path_start, dev_indices_filename)
+TEST_INDICES_PATH = os.path.join(path_start, test_indices_filename)
 
 def get_indices_to_exclude(filename, keyword_to_exclude="kolumni"):
     exclude = set()
@@ -35,11 +43,16 @@ def get_random_indices(range_stop:int, n=5000, seed=333, exclude:list|set=None):
 
     return set(rng.choice(indices, size=n, replace=False))
 
-indices_to_exclude, last_index = get_indices_to_exclude(DATA_PATH)
-dev_indices = get_random_indices(last_index, exclude=indices_to_exclude)
+range_stop = get_line_count(DATA_PATH)
+dev_indices = get_random_indices(range_stop)
 
-with open(DATA_PATH) as read_from, open(DEVTEST_PATH, "w") as write_to:
+with open(DATA_PATH) as read_from, open(DEVTEST_PATH, "w") as write_to, open(DEV_INDICES_PATH, "w") as write_dev_i, open(TEST_INDICES_PATH, "w") as write_test_i:
     for i, line in tqdm(enumerate(read_from)):
         obj = json.loads(line)
-        obj["split"] = "dev" if i in dev_indices else "test"
+        if i in dev_indices:
+            obj["split"] = "dev"
+            print(i, file=write_dev_i)
+        else:
+            obj["split"] = "test"
+            print(i, file=write_test_i)
         print(json.dumps(obj, ensure_ascii=False), file=write_to)
