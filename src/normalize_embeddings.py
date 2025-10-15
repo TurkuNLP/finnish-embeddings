47d49ce60b15f663066c6f863c264f9ff5dcb356
Siiri K <siakap@utu.fi>
2025-10-15 10:06:17 +0300

    Normalize existing embeddings and save to file

---
+++ src/normalize_embeddings.py
@@ -0,0 +1,27 @@
+import faiss
+import numpy as np
+import argparse
+from config.init_logger import set_up_logger
+
+def main(args):
+
+    new_filename = args.embeddings_filename.replace(".npy", "_normalized.npy")
+    logger.info(f"Reading embeddings from {args.embeddings_filename} and writing them normalized to {new_filename}")
+
+    with open(args.embeddings_filename, "rb") as read_from, open(new_filename, "wb") as write_to:
+        embeddings = np.load(read_from)
+        logger.info(f"Loaded embeddings with shape {embeddings.shape}")
+        faiss.normalize_L2(embeddings)
+        logger.info(f"Sum of first row: {np.linalg.norm(embeddings[0, :])} (expected 1)")
+        logger.info(f"Sum of first column: {np.linalg.norm(embeddings[:, 0])} (not expected to be 1)")
+        np.save(write_to, embeddings)
+
+if __name__ == "__main__":
+    
+    logger = set_up_logger(3)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("embeddings_filename")
+    args = parser.parse_args()
+
+    main(args)
