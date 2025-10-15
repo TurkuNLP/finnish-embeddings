c254e2cd2e7509f9013e1f68dfba5b49f453e43d
Siiri K <siakap@utu.fi>
2025-10-15 10:15:20 +0300

    Add bm25s to models to run

--- src/save_dev_top_100.py
+++ src/save_dev_top_100.py
@@ -1,10 +1,11 @@
 import os
 import logging
 from dotenv import load_dotenv
-from utils.helpers import yield_dev_indices, yield_dev_titles
+from utils.helpers import yield_values_from_jsonl, get_line_count, yield_indices_by_split, yield_values_by_split
 from embed import BatchEmbedder
 from index import load_index
 from query import query
+from run_bm25s import run_bm25s
 import numpy as np
 
 def set_up_logger(verbosity_level:int):
@@ -36,6 +37,10 @@ class XlmConfig:
         self.max_tokens_per_batch = -1 # stands for undefined
         self.batch_size = 512
 
+class BM25Config:
+    def __init__(self):
+        self.model_name = "bm25s"
+
 def get_index_path(index_dir, model_name):
     return os.path.join(index_dir, f"{model_name.replace("/", "__")}_indexIP.faiss") # get FlatIP index, not FlatL2
 
@@ -55,41 +60,56 @@ def main():
 
     k_nearest = 100
 
-    query_indices = list(yield_dev_indices(DATA_PATH))
+    query_indices = list(yield_indices_by_split(DATA_PATH))
 
     configs = (
         QwenConfig(),
         E5Config(),
         BertConfig(),
-        XlmConfig())
+        XlmConfig(),
+        BM25Config(),
+        )
 
     for config in configs:
 
         logger.info(f"Starting the run with {config.model_name}")
 
-        read_index_from = get_index_path(INDEX_DIR, config.model_name)
-        index = load_index(read_index_from)
-        logger.info(f"Index loaded from {read_index_from} with shape ({index.ntotal}, {index.d})")
+        if config.model_name == "bm25s":
+            result_indices, distances = run_bm25s(
+                passages=yield_values_from_jsonl(DATA_PATH, key="text_end"),
+                corpus_len=get_line_count(DATA_PATH),
+                queries=yield_values_by_split(DATA_PATH, value_key="title", split="dev"),
+                queries_len=len(query_indices),
+                save_index_to="",
+                top_k_list=[k_nearest],
+                language="finnish"
+            )
 
-        batch_embedder = BatchEmbedder(
-            model_name=config.model_name,
-            batch_size=config.batch_size,
-            max_tokens_per_batch=config.max_tokens_per_batch)
-        
-        encoded_queries = batch_embedder.encode_queries(
-            documents=yield_dev_titles(DATA_PATH),
-            num_documents=len(query_indices),
-            return_embeddings=True)
-        
-        distances, result_indices = query(index, encoded_queries, k_nearest)
+        else:
+            read_index_from = get_index_path(INDEX_DIR, config.model_name)
+            index = load_index(read_index_from)
+            logger.info(f"Index loaded from {read_index_from} with shape ({index.ntotal}, {index.d})")
+
+            batch_embedder = BatchEmbedder(
+                model_name=config.model_name,
+                batch_size=config.batch_size,
+                max_tokens_per_batch=config.max_tokens_per_batch)
+            
+            encoded_queries = batch_embedder.encode_queries(
+                documents=yield_values_by_split(DATA_PATH),
+                num_documents=len(query_indices),
+                return_embeddings=True)
+            
+            distances, result_indices = query(index, encoded_queries, k_nearest)
+
+            del batch_embedder # clear memory before using another model
+
         distances_path, indices_path = get_results_paths(RESULTS_DIR, config.model_name) 
         with open(distances_path, "bw") as d, open(indices_path, "bw") as i:
             np.save(d, distances)
             np.save(i, result_indices)
         logger.info(f"Distance matrix saved to {distances_path}")
         logger.info(f"Result indices matrix saved to {indices_path}")
-
-        del batch_embedder # clear memory before using another model
 
 
 if __name__ == "__main__":
