ea9296511b1e3293b1a800e2aa2d432474a7a54d
Siiri K <siakap@utu.fi>
2025-11-05 10:08:09 +0200

    Add short names for models

--- config/ModelConfig.py
+++ config/ModelConfig.py
@@ -6,11 +6,13 @@ class ModelConfig(ABC):
                  batch_size: int = -1,
                  max_tokens_per_batch: int = -1,
                  default_prompt: str = "",
+                 short_name: str = None
                  ):
         self.model_name = model_name
         self.batch_size = batch_size
         self.max_tokens_per_batch = max_tokens_per_batch
         self.default_prompt = default_prompt
+        self.short_name = short_name
 
     # Return the attributes of the object as a string
     def __str__(self):
@@ -23,6 +25,7 @@ class QwenConfig(ModelConfig):
             model_name = "Qwen/Qwen3-Embedding-8B",
             max_tokens_per_batch = 65000,
             default_prompt = "Retrieval the relevant passage for the given query", # from https://github.com/QwenLM/Qwen3-Embedding/blob/main/evaluation/task_prompts.json
+            short_name = "qwen"
         )
 
 class E5Config(ModelConfig):
@@ -31,25 +34,29 @@ class E5Config(ModelConfig):
             model_name = "intfloat/multilingual-e5-large-instruct",
             max_tokens_per_batch = 500000,
             default_prompt = "Given a scientific paper title, retrieve paper abstracts that are cited by the given paper", # closest to the task at hand, from https://github.com/microsoft/unilm/blob/9c0f1ff7ca53431fe47d2637dfe253643d94185b/e5/utils.py#L106
+            short_name = "e5"
         )
 
 class BertConfig(ModelConfig):
     def __init__(self):
         super().__init__(
             model_name = "TurkuNLP/bert-base-finnish-cased-v1",
-            batch_size = 1024
+            batch_size = 1024,
+            short_name = "bert"
         )
 
 class XlmConfig(ModelConfig):
     def __init__(self):
         super().__init__(
             model_name = "sentence-transformers/paraphrase-xlm-r-multilingual-v1",
-            batch_size = 512
+            batch_size = 512,
+            short_name = "xlm"
         )
 
 class BM25Config(ModelConfig):
     def __init__(self):
         super().__init__(
-            model_name = "bm25s"
+            model_name = "bm25s",
+            short_name = self.model_name
         )
         
