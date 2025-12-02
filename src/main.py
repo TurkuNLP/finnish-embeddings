bab02de9742059ff77c750fbfd04c92ad9caeacf
Siiri K <siakap@utu.fi>
2025-12-02 13:44:09 +0200

    Update logging method

--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
+from config.set_up_logging import set_up_logging
 from config.init_argument_parser import init_argument_parser
 from config.Config import Config
 import logging
 from src.run_pipeline import run_pipeline
 from src.run_final_evaluation import run_final_evaluation
 
-def set_up_logger(verbosity_level:int):
-    verbosity_levels = {0: logging.CRITICAL, 1: logging.WARNING, 2: logging.INFO, 3: logging.DEBUG}
-    logging.basicConfig(level=verbosity_levels[verbosity_level], force=True)
-    return logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 def log_args(config):
     logger.info(config)
@@ -26,6 +24,6 @@ if __name__ == "__main__":
     parser = init_argument_parser()
     args = parser.parse_args()
     config = Config.parse_config(args)
-    logger = set_up_logger(config.verbosity_level)
+    set_up_logging(config.verbosity_level)
 
     main(config)
