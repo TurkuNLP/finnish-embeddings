4ffc7109e5062e8ac2cf2a61ca9592d08977f9dd
Siiri K <siakap@utu.fi>
2025-12-02 13:42:52 +0200

    Add a path for saving result matrices

--- config/Config.py
+++ config/Config.py
@@ -21,6 +21,7 @@ class Config:
     save_embeddings_to: str = None
     save_index_to: str = None
     save_results_to: str = None
+    save_matrices_to: str = os.getenv("EVAL_DIR")
 
     # Data extraction
     passage_key: str = "text_end"
@@ -34,7 +35,7 @@ class Config:
     language: str = "finnish"
 
     # Evaluation
-    top_k: List[int] = field(default_factory=lambda: [1, 5]) # How many articles to retrieve in evaluation
+    top_k: List[int] = field(default_factory=lambda: [1, 5, 10, 25, 50, 100]) # How many articles to retrieve in evaluation
     first_k : int = 5 # How many retrieval results to show in text
 
     # Logging
@@ -55,21 +56,21 @@ class Config:
         if "bm25" in self.model_name:
             if self.test:
                 self.save_index_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/index/'
-                self.save_results_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/results_test.json' # TODO: Modify once the format is decided
+                self.save_results_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/results_test.json'
             else:
                 self.save_index_to: str = f'{os.getenv("INDEX_DIR")}/{self.replace_slashes_in_model_name()}/index/'
-                self.save_results_to: str = f'{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_results.json' # TODO: Modify once the format is decided
+                self.save_results_to: str = f'{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_results.json'
             return
 
         if self.test:
             self.save_embeddings_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/embeddings_test.npy'
             self.save_index_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/index_test.faiss'
-            self.save_results_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/results_test.json' # TODO: Modify once the format is decided
+            self.save_results_to: str = f'{os.getenv("TEST_DIR")}/{self.replace_slashes_in_model_name()}/results_test.json'
 
         else:
             self.save_embeddings_to: str = f'{os.getenv("EMBEDDING_DIR")}/{self.replace_slashes_in_model_name()}_embeddings_normalized.npy'
             self.save_index_to: str = f'{os.getenv("INDEX_DIR")}/{self.replace_slashes_in_model_name()}_indexIP.faiss'
-            self.save_results_to: str = f'{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_resultsIP.json' # TODO: Modify once the format is decided
+            self.save_results_to: str = f'{os.getenv("EVAL_DIR")}/{self.replace_slashes_in_model_name()}_resultsIP.json'
 
     @classmethod
     def parse_config(cls, args):
